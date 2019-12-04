fe8b260dec472ba18a4bcd629e24634ea01a5d37
愿雄 刘 <1835421485@qq.com>
2019-12-04 17:21:15 +0800

    添加项目文件。

---
+++ test/Program.cs
@@ -0,0 +1,28 @@
+﻿using algorithm;
+using System;
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] arr = new float[] { 9.54f, 8.45f, 7.45f, 6.45f, 5.45f, 5.45f, 4.45f, 3.45f, 2, 1, 1 };
+            QSort<float> qSort = new QSort<float>(arr);
+            DateTime beforDT = DateTime.Now;
+
+            qSort.Sort();
+            foreach (var i in arr)
+            {
+                Console.WriteLine(i);
+            }
+
+            DateTime afterDT = DateTime.Now;
+            TimeSpan ts = afterDT.Subtract(beforDT);
+            Console.WriteLine("DateTime总共花费{0}ms.", ts.TotalMilliseconds);
+
+            
+           
+            Console.Read();
+        }
+    }
+}
